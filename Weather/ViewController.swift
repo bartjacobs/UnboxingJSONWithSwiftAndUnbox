a23ebf857b844fc75e43700307bdf09552d95fb2
Bart Jacobs <git@codefoundry.be>
2017-07-31 15:41:52 +0200

    Add ability to fetch weather data in ViewController

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -14,6 +14,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Fetch Weather Data
+        fetchWeatherData()
+    }
+
+    // MARK: - Helper Methods
+
+    private func fetchWeatherData() {
+        // Helpers
+        let sharedSession = URLSession.shared
+
+        let latitude = 51.525598
+        let longitude = -0.118750
+        let apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+        if let url = URL(string: "https://api.forecast.io/forecast/\(apiKey)/\(latitude),\(longitude)") {
+            // Create Data Task
+            let dataTask = sharedSession.dataTask(with: url, completionHandler: { (data, response, error) in
+                if let requestError = error {
+                    print("Unable to Fetch Weather Data")
+                    print("\(requestError), \(requestError.localizedDescription)")
+
+                } else if let weatherData = data {
+                    self.processWeatherData(weatherData)
+                }
+            })
+
+            dataTask.resume()
+        }
+    }
+
+    private func processWeatherData(_ data: Data) {
+        if let dataAsString = String(data: data, encoding: String.Encoding.utf8) {
+            print(dataAsString)
+        }
     }
 
 }
