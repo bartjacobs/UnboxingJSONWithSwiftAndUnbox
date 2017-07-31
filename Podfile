93f4e9dc2eb731c96dd18be5b7c70f13581612d5
Bart Jacobs <git@codefoundry.be>
2017-07-31 11:17:52 +0200

    Project and CocoaPods setup

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'Weather' do
+  platform :ios, '10.0'
+  use_frameworks!
+
+  pod 'Unbox'
+end
---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Bart Jacobs on 31/07/2017.
+//  Copyright © 2017 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
